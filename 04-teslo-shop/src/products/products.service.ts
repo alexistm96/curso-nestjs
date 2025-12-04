ad2418c35649bb8c6c8f6b7a741ecf3d89723d8f
alexistm96 <alexistm96@gmail.com>
2025-12-03 17:14:33 -0700

    get y delete en typeORM

--- 04-teslo-shop/src/products/products.service.ts
+++ 04-teslo-shop/src/products/products.service.ts
@@ -3,11 +3,12 @@ import {
   Injectable,
   InternalServerErrorException,
   Logger,
+  NotFoundException,
 } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { Equal, Repository } from 'typeorm';
 import { Product } from './entities/product.entity';
 
 @Injectable()
@@ -31,19 +32,26 @@ export class ProductsService {
   }
 
   findAll() {
-    return `This action returns all products`;
+    const products = this.productRepository.find();
+    // if (!products || products.length === 0)
+    //   throw new NotFoundException('No products found');
+    return products;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} product`;
+  async findOne(id: string) {
+    const product = await this.productRepository.findOneBy({ id: Equal(id) });
+    if (!product)
+      throw new NotFoundException(`Product with id ${id} not found.`);
+    return product;
   }
 
   update(id: number, updateProductDto: UpdateProductDto) {
     return `This action updates a #${id} product`;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} product`;
+  async remove(id: string) {
+    const product = await this.findOne(id);
+    const deleteResult = await this.productRepository.remove(product);
   }
 
   private handleExceptions(error: any) {
